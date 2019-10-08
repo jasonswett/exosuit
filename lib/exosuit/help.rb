7c18ce94023154f646e14e2006253f22da1d60ba
Jason Swett <jason@benfranklinlabs.com>
2019-10-08 15:38:25 -0400

    Update help text.

--- lib/exosuit/help.rb
+++ lib/exosuit/help.rb
@@ -10,7 +10,8 @@ These are the commands you can use:
   instances              Show a summary of all running EC2 instances
   instances:all          Show a summary of EC2 instances (all states)
   dns                    List public DNS names for all EC2 instances
-  ssh                    SSH into an EC2 instance)
+  ssh                    SSH into an EC2 instance
+  initialize             Install Ruby and nginx on an instance)
     end
   end
 end

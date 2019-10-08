77fb246e9a3bda66aa572497bbe49e8c7ba2c42c
Jason Swett <jason@benfranklinlabs.com>
2019-10-08 15:36:46 -0400

    Get nginx installation to work.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,3 +10,18 @@ gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A170
 curl -L get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 rvm install 2.6.3
+
+# --------------------------------
+
+sudo apt-get install -y nginx
+
+sudo apt-get install -y dirmngr gnupg
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+sudo apt-get install -y apt-transport-https ca-certificates
+
+sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger bionic main > /etc/apt/sources.list.d/passenger.list'
+sudo apt-get update
+
+sudo apt-get install -y libnginx-mod-http-passenger
+
+sudo service nginx restart
